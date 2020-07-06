a7c10735e2b733cb08d6570efa19b0df478f75ee
Snigi13 <65667637+Snigi13@users.noreply.github.com>
2020-07-06 16:28:34 +0530

    Add files via upload

--- bob.js
+++ bob.js
@@ -5,19 +5,21 @@ class bob {
             'friction':1.0,
             'density':1.0
         }
-        this.body =Bodies.circle(this.x,this.y,this.radius,options);
+       
         this.x = x;
         this.y = y;
         this.radius=radius;
+        this.body =Bodies.circle(this.x,this.y,this.radius,options);
 
         //this.body = height;
-        World.add(world,this.body);
+        World.add(world, this.body);
       }
       display(){
         push();
         translate(this.body.position.x, this.body.position.y);
-        ellipseMode(RADIUS) 
         ellipse(0,0,this.radius,this.radius)
+        ellipseMode(RADIUS) 
+
         pop();
       }
 }
--- sketch.js
+++ sketch.js
@@ -1,49 +1,642 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground 
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+const Body = Matter.Body;
 
-
- var engine, world;
-var gamestate = "ONSLING" 
+function preload()
+{
+	
+}
 
 function setup() {
-  createCanvas(800,400); 
+	createCanvas(800, 700);
+
+  bob1 = new bob (50,300,30);
+  bob2 = new bob (100,300,30);
+  bob3 = new bob (150,300,30);
+  bob4 = new bob (200,300,30);
+  bob5 = new bob (250,300,30); 
+  bobDiameter=40; 
+  rope1 = new Chain (bob1.body,roof1.body,bobDiameter*2,0);
+  rope2 = new Chain (bob2.body,roof1.body,bobDiameter*2,0);
+  rope3 = new Chain (bob3.body,roof1.body,bobDiameter*2,0);
+  rope4 = new Chain (bob4.body,roof1.body,bobDiameter*2,0);
+  rope5 = new Chain (bob5.body,roof1.body,bobDiameter*2,0);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
+	//World.add(world, packageBody);
+	
+
+	//Create a Ground
+	roof = Bodies.rectangle(200,50, width, 10 , {isStatic:true} );
+ 	//World.add(world,roof);
+
+
+	Engine.run(engine);
   
-  engine = Engine.create();
-  world = engine.world;
 }
+
 
 function draw() {
-  background(255,255,255);  
+  rectMode(CENTER);
+  background(0); 
+
+  Engine.update(engine);
   
-  box1=new box (330,235,30,40)
-  box2= new box(360,235,30,40)
-  box3=new box(390,235,30,40)
-  box4=new box(420,235,30,40)
-  box5=new box(450,235,30,40)
-  box6=new box(360,195,30,40)
-  box7=new box(390,195,30,40)
-  box8=new box(420,195,30,40)
-  box9=new box(390,155,30,40)
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
 
- polygon= Bodies.circle(50,200,20); 
- World.add(world,polygon);
-
- sling = new SlingShot(this.polygon,{x:100,y:200}) 
-
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
 
   drawSprites();
-} 
-function mouseDragged(){
-  if(gamestate!== "launched"){
-  Matter.Body.setPosition(polygon.body, {x: mouseX , y: mouseY});
-  }
+ 
 }
 
 
-function mouseReleased(){
 
-  sling.fly();
-  gamestate="launched"  
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
